b63378709fee852b26e54065b92de7cee7d5c58d
kipal42 <kipal42@gmail.com>
2014-03-09 19:07:17 +0100

    module on

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+var blacklist = /.project|.md$|.*\.git.*|.*\.settings.*|.*node_modules.*/;
 var requireDirectory = require('require-directory')
     Contour          = {};
 
 
-module.exports = requireDirectory(module);
+module.exports = requireDirectory(module, __dirname, blacklist);
